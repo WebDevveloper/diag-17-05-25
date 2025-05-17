549f95d9af618bcd4a6412e1cb83645f8f8ba167
WebDevveloper <truall562gensf@gmail.com>
2025-05-17 21:39:04 +0300

    part-3

---
+++ mini-shop/mini-shop/src/components/ProductCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ProductCard() {
+  return (
+    <div>
+        <h3>ProductCard</h3>
+
+        <div>
+            <Image src='./giorgio-trovato-K62u25Jk6vo-unsplash.jpg'></Image>
+        </div>
+    </div>
+  )
+}
